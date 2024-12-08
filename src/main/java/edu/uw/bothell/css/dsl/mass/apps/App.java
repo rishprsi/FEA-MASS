5348ad4280fdff1e39f78b645ffd381bf3e63275
Rishabh Singh <rishprsi@cssmpi1h.uwb.edu>
2024-12-08 12:30:17 -0800

    first-commit

---
+++ src/main/java/edu/uw/bothell/css/dsl/mass/apps/App.java
@@ -0,0 +1,13 @@
+package edu.uw.bothell.css.dsl.mass.apps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
